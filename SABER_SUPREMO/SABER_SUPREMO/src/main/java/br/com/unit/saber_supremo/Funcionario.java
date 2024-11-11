05c82372f046921af10ee468bbe0f0ed476a49d1
Wagner Rodrigues <109499969+WagnerRodriguess@users.noreply.github.com>
2024-11-11 17:43:43 -0300

    Update Funcionario.java

--- SABER_SUPREMO/SABER_SUPREMO/src/main/java/br/com/unit/saber_supremo/Funcionario.java
+++ SABER_SUPREMO/SABER_SUPREMO/src/main/java/br/com/unit/saber_supremo/Funcionario.java
@@ -1,6 +1,6 @@
 
 package br.com.unit.saber_supremo;
-
+import java.util.Scanner;
 
 public class Funcionario extends Pessoa {
     
@@ -9,15 +9,65 @@ public class Funcionario extends Pessoa {
     private String cargo;
     private int nivel;
 
-    public Funcionario(String nome, String rg, String cpf, int anoNasc, int mesNasc, int diaNasc, String sexo, int matricula, String setor, String cargo, int nivel) {
-        super(nome, rg, cpf, anoNasc, mesNasc, diaNasc, sexo);
+    public void setMatricula(int matricula) {
         this.matricula = matricula;
+    }
+
+    public void setSetor(String setor) {
         this.setor = setor;
     }
 
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public Funcionario() {
+    }
+    
+
     @Override
     public void cadastrar() {
-     
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digie o nome do Funcionario: ");
+        setNome(sc.nextLine());
+        
+        System.out.println("Digite o RG do Funcionario: ");
+        setRg(sc.nextLine());
+        
+        System.out.println("Digite o CPF do Funcionario");
+        setCpf(sc.nextLine());
+        
+        System.out.println("Digite o ano de nascimento do Funcionario: ");
+        setAnoNasc(sc.nextInt());
+        
+        System.out.println("Digite o mes de nascimento do Funcionario: ");
+        setMesNasc(sc.nextInt());
+        
+        System.out.println("Digite o dia de nascimento do Funcionario: ");
+        setDiaNasc(sc.nextInt());
+        
+        System.out.println("Digite o sexo do Funcionario: ");
+        setSexo(sc.nextLine());
+       
+        System.out.println("Digite a matricula do Funcionario: ");
+        setMatricula(sc.nextInt());
+        
+        System.out.println("Digite o setor do Funcionario: ");
+        setSetor(sc.nextLine());
+        
+        System.out.println("Digite o cargo do Funcionario: ");
+        setCargo(sc.nextLine());
+        
+        System.out.println("Digite o nivel do Funcionario: ");
+        setNivel(sc.nextInt());
+      
+ 
     }
 
     @Override
@@ -28,10 +78,14 @@ public class Funcionario extends Pessoa {
         System.out.println("CPF: "+ getCpf());
         System.out.println("Data de Nascimento: "+ getDiaNasc() + "/"+ getMesNasc()+ "/"+ getAnoNasc());
         System.out.println("Sexo: "+ getSexo());
-        System.out.println("");
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Setor: " + setor);
+        System.out.println("Cargo" + cargo);
+        System.out.println("Nivel: "+ nivel);
         
     }
     
     
     
 }
+
