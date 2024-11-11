64d39fe1599beab7daa1ad512a244a9f350062f4
ArthurBastos2004 <136352261+ArthurBastos2004@users.noreply.github.com>
2024-11-11 17:45:10 -0300

    Update CoordenadorADM.java

--- SABER_SUPREMO/SABER_SUPREMO/src/main/java/br/com/unit/saber_supremo/CoordenadorADM.java
+++ SABER_SUPREMO/SABER_SUPREMO/src/main/java/br/com/unit/saber_supremo/CoordenadorADM.java
@@ -2,14 +2,14 @@
 package br.com.unit.saber_supremo;
 
 public class CoordenadorADM extends Funcionario {
-    public CoordenadorAdministrativo(String nome, int idade, String nivel) {
+    public CoordenadorADM(String nome, int idade, String nivel) {
         super(nome, idade, nivel);
     }
 
     @Override
     public double calcularSalario() {
         double salarioBase = super.calcularSalario();
-        return salarioBase * 1.135; // Bonificação de 13,5%
+        return salarioBase * 1.135; 
    
     }
 }
