2fe3a20b1213dc90c32cf985c6d1b182116d1fdd
Yuri <anthony.yuri.ff@gmail.com>
2024-11-11 17:38:04 -0300

    Update Curso.java

--- SABER_SUPREMO/SABER_SUPREMO/src/main/java/br/com/unit/saber_supremo/Curso.java
+++ SABER_SUPREMO/SABER_SUPREMO/src/main/java/br/com/unit/saber_supremo/Curso.java
@@ -1,6 +1,61 @@
-
 package br.com.unit.saber_supremo;
 
 public class Curso {
+    private String titulo;
+    private String descricao;
+    private Double valor;
+    private String sala;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+
+    public Curso(String titulo, String descricao, Double valor, String sala) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.sala = sala;
+    }
     
+    public void exibirCurso() {
+        System.out.println("Titulo: " + this.titulo);
+        System.out.println("Descricao: " + this.descricao);
+        System.out.println("Valor: " + this.valor);
+        System.out.println("Sala: " + this.sala);
+    }
+    
+    public void calcularNumMinAluno() {
+        double professorSalario = 8568.43;
+        double valorCurso = 865.23;
+        int minAlunos = (int) Math.ceil(professorSalario / valorCurso);
+        System.out.println("Número mínimo de alunos: " + minAlunos);
+    }
 }
